21b91f77c46e9d3275f9ab8369080b59713b178b
Tobi148 <tobioladoyin18@gmail.com>
2024-06-18 01:02:23 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const toggleMenu = document.querySelector(".toggleMenu");
+    const navigation = document.querySelector(".toggleMenu");
+    toggleMenu.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
